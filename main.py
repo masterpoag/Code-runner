34e8ecd355b5f77aac7b593a6e04b979f3d20989
masterpoag <colby.poag@gmail.com>
2024-04-10 16:28:56 -0500

    worked on config file and added support for more then one directory

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import os 
+CONFIG_LAYOUT = f""" # Location of directories with code in them for more then 1 file sperate with commas.
+Code/
+# Compile commands change or add to what you use
+c
+gcc -o {{FILENAME}} {{FILE}}
+c++
+g++ -o {{FILENAME}} {{FILE}}
+java
+javac {{FILE}}
+python
+python -m py_compile {{FILE}}
+c#
+csc {{FILE}}
+"""
+
 FOLDER_LOC= 0
 FILE_LIST = []
 
 def configReader():
-    global FOLDER_LOC
+    global FOLDER_LOC,CONFIG_LAYOUT
     try:
-        FOLDER_LOC = open("config.cfg").read()
+        FOLDER_LOC = open("config.cfg").readlines()[1].strip().split(",")
     except:
         f = open("config.cfg", "w")
-        f.write("Code/")
+        f.write(CONFIG_LAYOUT)
         f.close()
         configReader()
     else:
-        filesearch(FOLDER_LOC)
+        for i in FOLDER_LOC:
+            filesearch(i)
 
 def filesearch(f):
     global FILE_LIST
@@ -25,3 +41,6 @@ def filesearch(f):
             filesearch(f+i+"/")
 configReader()
 print(FILE_LIST)
+
+# testing
+# print(open("config.cfg").readlines()[1].split(","))
